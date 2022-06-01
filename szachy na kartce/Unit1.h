de5ba4aa7a7ff3c2a7440a855e29f5244243512c
Aleksandra Górska <aleksandra.gorska02@gmail.com>
2022-06-01 14:20:46 +0200

    Add files via upload

---
+++ szachy na kartce/Unit1.h
@@ -0,0 +1,116 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+#include <Menus.hpp>
+#include <ExtCtrls.hpp>
+#include <jpeg.hpp>
+#include <Graphics.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+        TMainMenu *MainMenu1;
+        TMenuItem *Jakgrac1;
+        TImage *p1;
+        TImage *p2;
+        TImage *p3;
+        TImage *p4;
+        TImage *p5;
+        TImage *p6;
+        TImage *p7;
+        TImage *p8;
+        TImage *p9;
+        TImage *p10;
+        TImage *p11;
+        TImage *p12;
+        TImage *p13;
+        TImage *p14;
+        TImage *p15;
+        TImage *p16;
+        TImage *p17;
+        TImage *p18;
+        TImage *p19;
+        TImage *p20;
+        TImage *p21;
+        TImage *p22;
+        TImage *p23;
+        TImage *p24;
+        TImage *p25;
+        TImage *p26;
+        TImage *p27;
+        TImage *p28;
+        TImage *p29;
+        TImage *p30;
+        TImage *p31;
+        TImage *p32;
+        TImage *p33;
+        TImage *p34;
+        TImage *p35;
+        TImage *p36;
+        TImage *p37;
+        TImage *p38;
+        TImage *p39;
+        TImage *p40;
+        TImage *p41;
+        TImage *p42;
+        TImage *tura;
+        TImage *Image1;
+        TImage *Image2;
+        void __fastcall Jakgrac1Click(TObject *Sender);
+        void __fastcall FormCreate(TObject *Sender);
+        void __fastcall p1Click(TObject *Sender);
+        void __fastcall p2Click(TObject *Sender);
+        void __fastcall p3Click(TObject *Sender);
+        void __fastcall p4Click(TObject *Sender);
+        void __fastcall p5Click(TObject *Sender);
+        void __fastcall p6Click(TObject *Sender);
+        void __fastcall p7Click(TObject *Sender);
+        void __fastcall p8Click(TObject *Sender);
+        void __fastcall p9Click(TObject *Sender);
+        void __fastcall p10Click(TObject *Sender);
+        void __fastcall p11Click(TObject *Sender);
+        void __fastcall p12Click(TObject *Sender);
+        void __fastcall p13Click(TObject *Sender);
+        void __fastcall p14Click(TObject *Sender);
+        void __fastcall p15Click(TObject *Sender);
+        void __fastcall p16Click(TObject *Sender);
+        void __fastcall p17Click(TObject *Sender);
+        void __fastcall p18Click(TObject *Sender);
+        void __fastcall p19Click(TObject *Sender);
+        void __fastcall p20Click(TObject *Sender);
+        void __fastcall p21Click(TObject *Sender);
+        void __fastcall p22Click(TObject *Sender);
+        void __fastcall p23Click(TObject *Sender);
+        void __fastcall p24Click(TObject *Sender);
+        void __fastcall p25Click(TObject *Sender);
+        void __fastcall p26Click(TObject *Sender);
+        void __fastcall p27Click(TObject *Sender);
+        void __fastcall p28Click(TObject *Sender);
+        void __fastcall p29Click(TObject *Sender);
+        void __fastcall p30Click(TObject *Sender);
+        void __fastcall p31Click(TObject *Sender);
+        void __fastcall p32Click(TObject *Sender);
+        void __fastcall p33Click(TObject *Sender);
+        void __fastcall p34Click(TObject *Sender);
+        void __fastcall p35Click(TObject *Sender);
+        void __fastcall p36Click(TObject *Sender);
+        void __fastcall p37Click(TObject *Sender);
+        void __fastcall p38Click(TObject *Sender);
+        void __fastcall p39Click(TObject *Sender);
+        void __fastcall p40Click(TObject *Sender);
+        void __fastcall p41Click(TObject *Sender);
+        void __fastcall p42Click(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+        __fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
